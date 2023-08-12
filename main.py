5223c85ee7f4cb3f7df3b67b989a3d9608e415a0
E3Tmn <nick1tapinyagin@yandex.ru>
2023-08-12 13:07:57 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,77 @@
+import file_operations
+from faker import Faker
+import random
+
+
+fake = Faker("ru_RU")
+
+
+def create_wonder_skills():
+    skills = [
+        'Стремительный прыжок',
+        'Электрический выстрел',
+        'Ледяной удар',
+        'Стремительный удар',
+        'Кислотный взгляд',
+        'Тайный побег',
+        'Ледяной выстрел',
+        'Огненный заряд',
+    ]
+    runic_alph = {
+        'а': 'а͠', 'б': 'б̋', 'в': 'в͒͠',
+        'г': 'г͒͠', 'д': 'д̋', 'е': 'е͠',
+        'ё': 'ё͒͠', 'ж': 'ж͒', 'з': 'з̋̋͠',
+        'и': 'и', 'й': 'й͒͠', 'к': 'к̋̋',
+        'л': 'л̋͠', 'м': 'м͒͠', 'н': 'н͒',
+        'о': 'о̋', 'п': 'п̋͠', 'р': 'р̋͠',
+        'с': 'с͒', 'т': 'т͒', 'у': 'у͒͠',
+        'ф': 'ф̋̋͠', 'х': 'х͒͠', 'ц': 'ц̋',
+        'ч': 'ч̋͠', 'ш': 'ш͒͠', 'щ': 'щ̋',
+        'ъ': 'ъ̋͠', 'ы': 'ы̋͠', 'ь': 'ь̋',
+        'э': 'э͒͠͠', 'ю': 'ю̋͠', 'я': 'я̋',
+        'А': 'А͠', 'Б': 'Б̋', 'В': 'В͒͠',
+        'Г': 'Г͒͠', 'Д': 'Д̋', 'Е': 'Е',
+        'Ё': 'Ё͒͠', 'Ж': 'Ж͒', 'З': 'З̋̋͠',
+        'И': 'И', 'Й': 'Й͒͠', 'К': 'К̋̋',
+        'Л': 'Л̋͠', 'М': 'М͒͠', 'Н': 'Н͒',
+        'О': 'О̋', 'П': 'П̋͠', 'Р': 'Р̋͠',
+        'С': 'С͒', 'Т': 'Т͒', 'У': 'У͒͠',
+        'Ф': 'Ф̋̋͠', 'Х': 'Х͒͠', 'Ц': 'Ц̋',
+        'Ч': 'Ч̋͠', 'Ш': 'Ш͒͠', 'Щ': 'Щ̋',
+        'Ъ': 'Ъ̋͠', 'Ы': 'Ы̋͠', 'Ь': 'Ь̋',
+        'Э': 'Э͒͠͠', 'Ю': 'Ю̋͠', 'Я': 'Я̋',
+        ' ': ' '
+    }
+    wonder_skills = []
+    skills_of_char = random.sample(skills, 3)
+    for skill in skills_of_char:
+        for letter in skill:
+            skill = skill.replace(letter, runic_alph[letter])
+        wonder_skills.append(skill)
+    return wonder_skills
+
+
+def create_character(number, skills):
+    character = {
+        "first_name": fake.first_name_male(),
+        "last_name": fake.last_name_male(),
+        'town': fake.city(),
+        'job': fake.job(),
+        'strength': random.randint(3, 18),
+        'agility': random.randint(3, 18),
+        'endurance': random.randint(3, 18),
+        'intelligence': random.randint(3, 18),
+        'luck': random.randint(3, 18),
+        'skill_1': skills[0],
+        'skill_2': skills[1],
+        'skill_3': skills[2],
+    }
+    file_operations.render_template("charsheet.svg", f"output/char_card{number}.svg", character)
+
+
+def main():
+    for i in range(1, 11):
+        create_character(i, create_wonder_skills())
+
+if __name__ == '__main__':
+    main()
